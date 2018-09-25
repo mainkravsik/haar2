edb01b96bd9e7455668ef0289e970da8315edc4f
mainkravsik <mainkravsik@gmail.com>
2018-09-25 13:31:36 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace haar2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.haarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.webcamToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.haarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.webcamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1332, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(12, 24);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem2,
+            this.haarToolStripMenuItem1,
+            this.webcamToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem2
+            // 
+            this.openToolStripMenuItem2.Name = "openToolStripMenuItem2";
+            this.openToolStripMenuItem2.Size = new System.Drawing.Size(216, 26);
+            this.openToolStripMenuItem2.Text = "Open";
+            this.openToolStripMenuItem2.Click += new System.EventHandler(this.openToolStripMenuItem2_Click);
+            // 
+            // haarToolStripMenuItem1
+            // 
+            this.haarToolStripMenuItem1.Name = "haarToolStripMenuItem1";
+            this.haarToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.haarToolStripMenuItem1.Text = "Haar";
+            this.haarToolStripMenuItem1.Click += new System.EventHandler(this.haarToolStripMenuItem1_Click);
+            // 
+            // webcamToolStripMenuItem1
+            // 
+            this.webcamToolStripMenuItem1.Name = "webcamToolStripMenuItem1";
+            this.webcamToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.webcamToolStripMenuItem1.Text = "Webcam";
+            this.webcamToolStripMenuItem1.Click += new System.EventHandler(this.webcamToolStripMenuItem1_Click);
+            // 
+            // haarToolStripMenuItem
+            // 
+            this.haarToolStripMenuItem.Name = "haarToolStripMenuItem";
+            this.haarToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.haarToolStripMenuItem.Text = "haar";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.openToolStripMenuItem1.Text = "open";
+            // 
+            // webcamToolStripMenuItem
+            // 
+            this.webcamToolStripMenuItem.Name = "webcamToolStripMenuItem";
+            this.webcamToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.webcamToolStripMenuItem.Text = "webcam";
+            
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1293, 629);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1332, 688);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haarToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem webcamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem haarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem webcamToolStripMenuItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
+
+namespace haar2
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> imgInput;
+        VideoCapture capture;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+       
+        public void DetectFaceHaar()
+        {
+            try
+            {
+                string facePath = Path.GetFullPath(@"../../data/haarcascade_frontalface_default.xml");
+                string eyePath = Path.GetFullPath(@"../../data/haarcascade_eye.xml");
+
+
+                CascadeClassifier classifierFace = new CascadeClassifier(facePath);
+                CascadeClassifier classifierEye = new CascadeClassifier(eyePath);
+
+                var imgGray = imgInput.Convert<Gray, byte>().Clone();
+                Rectangle[] faces = classifierFace.DetectMultiScale(imgGray, 1.1, 4);
+                foreach (var face in faces)
+                {
+                    imgInput.Draw(face, new Bgr(0, 0, 255), 2);
+
+                    imgGray.ROI = face;
+                    Rectangle[] eyes = classifierEye.DetectMultiScale(imgGray, 1.1, 4);
+                    foreach (var eye in eyes)
+                    {
+                        var e = eye;
+                        e.X += face.X;
+                        e.Y += face.Y;
+                        imgInput.Draw(e, new Bgr(0, 255, 0), 2);
+                    }
+                }
+                pictureBox1.Image = imgInput.Bitmap;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+       
+
+       
+
+        private void openToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    imgInput = new Image<Bgr, byte>(dialog.FileName);
+                    pictureBox1.Image = imgInput.Bitmap;
+                }
+                else
+                {
+                    throw new Exception("No file selected.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void haarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (imgInput == null)
+                {
+                    throw new Exception("Please select and image");
+                }
+
+                DetectFaceHaar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void webcamToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
